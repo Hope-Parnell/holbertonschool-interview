dff7434a9c6a66b624df1b3ddba10be75f2eecbf
Hope-Parnell <parnell.hope.c@gmail.com>
2022-06-08 00:19:37 -0500

    pycodestyle

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,6 +5,7 @@ Each box is numbered sequentially from '0' to 'n - 1'
 and each box may contain keys to the other boxes.
 """
 
+
 def canUnlockAll(boxes):
     """
         Tests if all boxes can be unlocked.
