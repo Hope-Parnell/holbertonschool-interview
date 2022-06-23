8e7bd411c5a267ec3b5481ee7369300c3aeb6a6b
Hope-Parnell <parnell.hope.c@gmail.com>
2022-06-22 19:53:20 -0500

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. 
-Given a number n, calculate the fewest number of operations needed to result in exactly n H characters in the file
+In a text file, there is a single character H. Your text editor can execute
+ only two operations in this file: Copy All and Paste. Given a number n,
+ calculate the fewest number of operations needed to result in exactly
+ n H characters in the file
 """
 from math import sqrt
 
+
 def minOperations(n):
     """
-    Finds the min number of Copy All and Pastes needed to have n H characters using prime factors
+    Finds the min number of Copy All and Pastes needed to have n H characters
     """
     if (n < 2):
         return 0
