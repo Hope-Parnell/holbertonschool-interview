d06d44198efd3dd992c2013c28a55c6ffd35f17d
Hope-Parnell <parnell.hope.c@gmail.com>
2022-11-16 12:28:42 -0600

    remove dup values from coins

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -2,13 +2,17 @@
 """given a list of coins make change for a given amont"""
 
 
+from audioop import reverse
+
+
 def makeChange(coins, total):
     """Return the minuimun number of coins needed to make change"""
     if (total <= 0):
         return 0
-    coinsNeeded = int(0)
+    coinsNeeded = 0
     coins.sort(reverse=True)
-    for coin in coins:
+    uniqueCoins = set(coins)
+    for coin in uniqueCoins:
         coinsNeeded += total // coin
         total %= coin
     return coinsNeeded if total == 0 else -1
