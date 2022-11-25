d1a5ec765291dfccdd4287b406f0c028bcd1c17d
Hope-Parnell <parnell.hope.c@gmail.com>
2022-11-25 12:54:44 -0600

    updated for if x or y are 0

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Island Perimeter"""
 
+
 def island_perimeter(grid):
     """
     Calculate perimeter of island given a grid
     where 1s represent land and 0s represent water
     """
     perimeter = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
+    height, width = len(grid), len(grid[0])
+    for x in range(height):
+        for y in range(width):
             if grid[x][y]:
-                # count number of 0's on left and top
-                perimeter += [grid[x - 1][y],
-                              grid[x][y - 1],
-                              ].count(0)
+                if not x or not grid[x - 1][y]:
+                    perimeter += 1
+                if not y or not grid[x][y - 1]:
+                    perimeter += 1
     return perimeter * 2
